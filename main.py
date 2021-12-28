19365e990627e3624db57b0a0c4077a9156e0d82
Maury Moreno <maury.moreno.pa@gmail.com>
2021-12-27 20:10:43 -0500

    Pruebas de coeficientes Binomiales

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def generate_diagonal(n, l):
-    # return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length
-    return 
+    # return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length  
+    data=[]
+    for i in range(n,n+l):   
+        data.append(combinatoria(i,n))   
+    return data
+ 
 
 def combinatoria(n,x):
     comb=factorial(n)/factorial(x)/factorial(n-x)
-    return comb
-    
+    return int(comb)
+
 def factorial(n):
     fact=1
     for i in range(1,n+1):
         fact = fact * i
     return fact
+  
