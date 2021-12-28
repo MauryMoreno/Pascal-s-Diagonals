a40524a7d1f846ccaef9935ef380d6f236776afb
Maury Moreno <maury.moreno.pa@gmail.com>
2021-12-27 19:02:00 -0500

    Creación de formulas auxiliares y pruebas

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 def generate_diagonal(n, l):
     # return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length
     return 
+
+def combinatoria(n,x):
+    comb=factorial(n)/factorial(x)/factorial(n-x)
+    return comb
+    
+def factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact = fact * i
+    return fact
